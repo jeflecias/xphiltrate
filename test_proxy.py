4900ba32d0742fc4ca5b7d7a1ab87672fc6c7c07
jeflecias <jefleciasg@gmail.com>
2026-03-30 05:28:25 -0400

    removed content-encoding header forwarding

--- test_proxy.py
+++ test_proxy.py
@@ -4,27 +4,32 @@ from urllib.parse import urljoin
 
 app = FastAPI()
 
+
 client = httpx.AsyncClient(follow_redirects=False)
-TARGET_BASE = "https://example.com"
+TARGET_BASE = "https://0a1f00e804d373e981fefc0400e5002c.web-security-academy.net/"
 
 
 HOP_BY_HOP_HEADERS = {
     "connection", "keep-alive", "proxy-authenticate", 
     "proxy-authorization", "te", "trailers", 
-    "transfer-encoding", "upgrade", "host", "content-length"
+    "transfer-encoding", "upgrade", "host", "content-length",
+    "content-encoding"
 }
 
 @app.api_route("/{path:path}", methods=["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD"])
 async def proxy(request: Request, path: str):
-
+   
     url = urljoin(TARGET_BASE.rstrip("/") + "/", path)
-    req_headers = {
-		k: v
-		for k, v in request.headers.items()
-		if k.lower() not in HOP_BY_HOP_HEADERS
-	}
+    
+   
+    req_headers = [
+        (k, v)
+        for k, v in request.headers.items()
+        if k.lower() not in HOP_BY_HOP_HEADERS
+    ]
             
     body = await request.body()
+    
     
     try:
         resp = await client.request(
@@ -34,21 +39,18 @@ async def proxy(request: Request, path: str):
             content=body,
             params=request.query_params
         )
-        
     except httpx.RequestError as exc:
         raise HTTPException(status_code=502, detail=f"Bad Gateway: {str(exc)}")
         
-
+   
     response = Response(
         content=resp.content,
         status_code=resp.status_code
     )
     
-    DROP_RESP_HEADERS = HOP_BY_HOP_HEADERS.union({"content-encoding"})
-    
+ 
     for k, v in resp.headers.multi_items():
-        if k.lower() not in DROP_RESP_HEADERS:
-            
+        if k.lower() not in HOP_BY_HOP_HEADERS:
             response.headers.append(k, v)
             
     return response
